170d51a33bd31fa95547ef55e7b47eb05ee3b234
Pratik Pathare <pratikbalasahebpathare@gmail.com>
2026-01-21 10:59:51 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql2");
 var util = require("util");
 
 var conn=mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "Root",
-    database : "hospital"
+    host : "b75nkye1dn2gbfqzndnj-mysql.services.clever-cloud.com",
+    user : "uoe9sreerws1diej",
+    password : "YqbQpr0FR5kWtwA5YydL",
+    database : "b75nkye1dn2gbfqzndnj"
 })
 
 var exe = util.promisify(conn.query).bind(conn)
